b3717e498225dae416171195fb7e2751a1696c03
Feramiz Bahaddinov <feramizbahadinnov@gmail.com>
2022-09-26 19:21:59 +0400

    added tailwind and react router

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+
+    },
+
+    extend: {
+      colors: {
+        'primary-bg': '#011027',
+        'primary-card': '#021D46',
+        'news-text': '#F3F3F3',
+        'news-empty-border': '#949494',
+        'primary-light': '#94B7EC',
+        'line': '#243652'
+      },
+    },
+  },
+  plugins: [],
+};

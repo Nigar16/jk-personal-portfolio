d856a812fdfbb9a7419614abbe866210798984bb
Feramiz Bahaddinov <feramizbahadinnov@gmail.com>
2022-10-10 23:39:20 +0400

    addedprojects

--- src/pages/projects/ProjectServices.jsx
+++ src/pages/projects/ProjectServices.jsx
@@ -7,7 +7,7 @@ const ProjectServices = () => {
   <div className="flex w-full lg:flex-col justify-end lg:items-center overflow-hidden">
     <div className="flex px-8">
       <img src={icon} />
-      <div className="pl-4 text-[#fff] ">
+      <div className="pl-4 text-[#fff] "> 
         <h1 className="text-[32px] font-[700]">500+</h1>
         <p className="text-[16px] font-[400]">Completed projects</p>
       </div>
